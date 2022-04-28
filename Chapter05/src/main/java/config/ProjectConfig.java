a707bc649d84bed0556fd252c7f129b23982f804
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-04-28 20:42:27 +0300

    Chapter05 - Bean Scope - Prototype

--- Chapter05/src/main/java/config/ProjectConfig.java
+++ Chapter05/src/main/java/config/ProjectConfig.java
@@ -1,10 +1,20 @@
 package config;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import service.CommentService;
 
 @Configuration
 @ComponentScan(basePackages = {"service"})
 public class ProjectConfig {
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public CommentService commentService() {
+        return new CommentService();
+    }
 
 }
--- Chapter05/src/main/java/service/CommentService.java
+++ Chapter05/src/main/java/service/CommentService.java
@@ -1,10 +1,5 @@
 package service;
 
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Service;
-
-@Service
-@Lazy
 public class CommentService {
 
     public CommentService() {
