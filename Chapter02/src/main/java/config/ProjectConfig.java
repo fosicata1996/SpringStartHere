fba35e944b8cfea7d98f2513c06ffb13d3ea910a
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-04-28 15:35:48 +0300

    Chapter02 - @Component

--- Chapter02/src/main/java/config/ProjectConfig.java
+++ Chapter02/src/main/java/config/ProjectConfig.java
@@ -2,17 +2,13 @@ package config;
 
 import main.Parrot;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "main")
 public class ProjectConfig {
-
-    @Bean
-    @Primary
-    Parrot parrot() {
-        return new Parrot("Koko");
-    }
 
     @Bean
     Parrot parrot2() {
--- Chapter02/src/main/java/main/Parrot.java
+++ Chapter02/src/main/java/main/Parrot.java
@@ -1,8 +1,18 @@
 package main;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Primary
 public class Parrot {
 
     private String name;
+
+    public Parrot() {
+    }
 
     public Parrot(String name) {
         this.name = name;
@@ -14,5 +24,11 @@ public class Parrot {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @PostConstruct
+    public void init() {
+        if (name == null)
+            this.name = "DefaultName";
     }
 }
