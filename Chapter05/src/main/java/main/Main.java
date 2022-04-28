13b2f8994cc3a2d5ad7f91e340bf77e2dd4c371c
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-04-28 20:29:04 +0300

    Chapter05 - Bean Scope - Singleton

---
+++ Chapter05/src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import service.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var cs1 = c.getBean("commentService", CommentService.class);
+        var cs2 = c.getBean("commentService", CommentService.class);
+
+        System.out.println(cs1 == cs2);
+    }
+}
