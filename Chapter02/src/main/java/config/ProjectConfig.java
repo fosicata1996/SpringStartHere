204e4d14ae20ee49dd4e4b733ce62b14e159e631
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-04-28 12:58:28 +0300

    Chapter02 - Multiple Spring Beans

--- Chapter02/src/main/java/config/ProjectConfig.java
+++ Chapter02/src/main/java/config/ProjectConfig.java
@@ -3,13 +3,25 @@ package config;
 import main.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
+    @Primary
     Parrot parrot() {
         return new Parrot("Koko");
+    }
+
+    @Bean
+    Parrot parrot2() {
+        return new Parrot("Miki");
+    }
+
+    @Bean("Riki")
+    Parrot parrot3() {
+        return new Parrot("Riki");
     }
 
     @Bean
--- Chapter02/src/main/java/main/Main.java
+++ Chapter02/src/main/java/main/Main.java
@@ -10,6 +10,12 @@ public class Main {
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
 
+        Parrot p2 = context.getBean("parrot2", Parrot.class);
+        System.out.println(p2.getName());
+
+        Parrot p3 = (Parrot) context.getBean("Riki");
+        System.out.println(p3.getName());
+
         String s = context.getBean(String.class);
         System.out.println(s);
 
