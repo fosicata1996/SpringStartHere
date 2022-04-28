7dfe5765f1ecb2035a2c83554d2ff8304ff2c099
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-04-28 20:02:28 +0300

    Chapter04 - @Service & @Repository

--- Chapter04/src/main/java/repositories/DBCommentRepository.java
+++ Chapter04/src/main/java/repositories/DBCommentRepository.java
@@ -1,9 +1,9 @@
 package repositories;
 
 import entities.Comment;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class DBCommentRepository implements CommentRepository {
     @Override
     public void storeComment(Comment comment) {
--- Chapter04/src/main/java/services/CommentService.java
+++ Chapter04/src/main/java/services/CommentService.java
@@ -3,11 +3,11 @@ package services;
 import entities.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
-@Component
+@Service
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
