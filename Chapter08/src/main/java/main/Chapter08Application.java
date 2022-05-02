2d2b80be9453ea1ce843bf88043a250e722ef8e5
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-02 15:24:44 +0300

    Chapter08 - First commit

---
+++ Chapter08/src/main/java/main/Chapter08Application.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter08Application.class, args);
+	}
+
+}
