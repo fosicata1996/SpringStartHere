5b30cefd8f90d7761fa07c655c45793b79d57f4c
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-03 10:16:30 +0300

    Chapter08 - @PathVariable

--- Chapter08/src/main/java/main/controllers/MainController.java
+++ Chapter08/src/main/java/main/controllers/MainController.java
@@ -2,15 +2,15 @@ package main.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
-    public String home(@RequestParam(required = false, defaultValue = "FOSI") String name,
-                       @RequestParam(required = false, defaultValue = "red") String color,
+    @RequestMapping("/{name}/{color}")
+    public String home(@PathVariable String name,
+                       @PathVariable String color,
                        Model page) {
         page.addAttribute("username", name);
         page.addAttribute("color", color);
