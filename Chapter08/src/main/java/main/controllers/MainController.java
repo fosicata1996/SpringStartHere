7ec2e483221cf6dfcc618ba829b5ea9083a01336
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-03 10:06:33 +0300

    Chapter08 - @RequestParam

--- Chapter08/src/main/java/main/controllers/MainController.java
+++ Chapter08/src/main/java/main/controllers/MainController.java
@@ -3,14 +3,17 @@ package main.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String home(Model page) {
-        page.addAttribute("username", "Fosi");
-        page.addAttribute("color", "red");
+    public String home(@RequestParam(required = false, defaultValue = "FOSI") String name,
+                       @RequestParam(required = false, defaultValue = "red") String color,
+                       Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
 
         return "home.html";
     }
