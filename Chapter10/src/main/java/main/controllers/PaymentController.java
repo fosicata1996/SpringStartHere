f74f8614550a16a9b4016056a9bbaddcf6f1fe84
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-04 11:53:00 +0300

    Chapter10 - @RequestBody

--- Chapter10/src/main/java/main/controllers/PaymentController.java
+++ Chapter10/src/main/java/main/controllers/PaymentController.java
@@ -1,26 +1,24 @@
 package main.controllers;
 
 import main.entities.PaymentDetails;
-import main.services.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
-    private final PaymentService paymentService;
-
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+    private static final Logger logger = Logger.getLogger(PaymentController.class.getName());
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-            PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<?> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        logger.info("Received payment " + paymentDetails.getAmount());
 
-            return ResponseEntity
-                    .status(HttpStatus.ACCEPTED)
-                    .body(paymentDetails);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
     }
 }
