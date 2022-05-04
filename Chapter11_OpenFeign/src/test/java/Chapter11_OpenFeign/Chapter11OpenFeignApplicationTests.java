13dc004b056b84bb4afdb195c893ba354aca2970
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-04 12:34:41 +0300

    Chapter11 - OpenFeign - Initial commit

---
+++ Chapter11_OpenFeign/src/test/java/Chapter11_OpenFeign/Chapter11OpenFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package Chapter11_OpenFeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter11OpenFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
