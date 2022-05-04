6f88fd5118e21344f002a0602bab35cd4721e57b
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-04 13:09:15 +0300

    Chapter11 - Revert

--- Chapter11_OpenFeign/src/main/java/main/ProjectConfig.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "main")
-public class ProjectConfig {
-}
