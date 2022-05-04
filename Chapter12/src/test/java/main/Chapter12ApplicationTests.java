a83f95f03f056de56bc18661ac07e505dbe42eac
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-04 14:26:49 +0300

    Chapter12 - Initial commit

---
+++ Chapter12/src/test/java/main/Chapter12ApplicationTests.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
