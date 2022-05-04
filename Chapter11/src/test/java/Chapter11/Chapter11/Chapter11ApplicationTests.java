eedc8e7a1b7890491588026435447d858c130b1b
Catalin.Fosalau <catalin.fosalau.ext@porscheinformatik.com>
2022-05-04 12:25:50 +0300

    Chapter11 - Initial commit

---
+++ Chapter11/src/test/java/Chapter11/Chapter11/Chapter11ApplicationTests.java
@@ -0,0 +1,13 @@
+package Chapter11.Chapter11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
